9320700442fbdc1a5dbbe18f408a50b76f6917ce
Pavan Manjunath <pavanbm17@gmail.com>
2024-05-09 23:12:21 +0530

    adding changeds to staging

---
+++ Pages/Deploymentslots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_appvs.Pages
+{
+    public class DeploymentslotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
